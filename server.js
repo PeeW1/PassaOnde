5f245589a7951bc12a99fa55896b87604b929267
PeeW1 <pedromonteiromm2@gmail.com>
2025-08-14 09:44:27 -0300

    alterando package.json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const saltRounds = 10;
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const db = mysql.createConnection({
